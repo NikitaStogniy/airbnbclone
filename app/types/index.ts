4a44090d15db3f397884c7f0e99d72310218e4ec
NikitaStogniyJafton <ggsailgg@gmail.com>
2023-07-01 14:36:07 +0300

    Listings, listing detail, listing reservation

--- app/types/index.ts
+++ app/types/index.ts
@@ -1,4 +1,4 @@
-import { User } from "@prisma/client";
+import { Listing, Reservation, User } from "@prisma/client";
 
 export type SafeUser = Omit<
 User,
@@ -6,4 +6,17 @@ User,
     createdAt: string;
     updatedAt: string;
     emailVerified: string | null;
+}
+
+export type SafeListing = Omit<Listing,
+"createdAt">&{
+    createdAt: string;
+}
+
+export type SafeReservation = Omit<Reservation,
+'createdAt' | 'startDate' | 'endDate'|'listing'> &{
+    createdAt: string,
+    startDate: string,
+    endDate: string,
+    listing: SafeListing
 }
