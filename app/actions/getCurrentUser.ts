663bb1ab69f00c42760fb8d84158c15872c6da1a
Nikita Stogniy <ggsailgg@gmail.com>
2023-07-16 16:52:00 +0300

    server err fix?

--- app/actions/getCurrentUser.ts
+++ app/actions/getCurrentUser.ts
@@ -1,37 +1,38 @@
-import { getServerSession } from "next-auth";
-import { authOptions } from "@/pages/api/auth/[...nextauth]";
+import { getServerSession } from "next-auth/next"
 
-import prisma from '@/app/libs/prismadb';
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
+import prisma from "@/app/libs/prismadb";
 
 export async function getSession() {
-    return await getServerSession(authOptions);
+  return await getServerSession(authOptions)
 }
 
 export default async function getCurrentUser() {
-    try{
-        const session = await getSession()
-        if(!session?.user?.email){
-            return null;
-        }
-        const currentUser = await prisma.user.findUnique({
-            where: {
-                email: session.user.email as string
-            }
-        });
+  try {
+    const session = await getSession();
 
-        if(!currentUser){
-            return null;
-        }
+    if (!session?.user?.email) {
+      return null;
+    }
 
-        return {
-            ...currentUser,
-        createdAt: currentUser.createdAt.toISOString(),
-        updatedAt: currentUser.updatedAt.toISOString(),
-        emailVerified: currentUser.emailVerified?.toISOString() || null,
-        }
+    const currentUser = await prisma.user.findUnique({
+      where: {
+        email: session.user.email as string,
+      }
+    });
+
+    if (!currentUser) {
+      return null;
     }
-    catch (error:any)
-    {
-        return null;
-    }
+
+    return {
+      ...currentUser,
+      createdAt: currentUser.createdAt.toISOString(),
+      updatedAt: currentUser.updatedAt.toISOString(),
+      emailVerified: 
+        currentUser.emailVerified?.toISOString() || null,
+    };
+  } catch (error: any) {
+    return null;
+  }
 }
