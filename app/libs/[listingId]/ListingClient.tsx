0d94060d7096472ccb093984746feffcf287b1c6
Nikita Stogniy <ggsailgg@gmail.com>
2023-07-16 15:53:49 +0300

    DateRange to Range

--- app/libs/[listingId]/ListingClient.tsx
+++ app/libs/[listingId]/ListingClient.tsx
@@ -12,6 +12,7 @@ import { differenceInCalendarDays, eachDayOfInterval, setDate } from "date-fns";
 import axios from "axios";
 import toast from "react-hot-toast";
 import ListingReservation from "@/app/components/Listings/ListingReservation";
+import { Range } from "react-date-range";
 
 const initialDateRange = {
 	startDate: new Date(),
@@ -51,8 +52,7 @@ const ListingClient: React.FC<ListingClientProps> = ({
 
 	const [isLoading, setIsLoading] = useState(false);
 	const [totalPrice, setTotalPrice] = useState(listing.price);
-	const [dateRange, setDateRange] = useState(initialDateRange);
-
+	const [dateRange, setDateRange] = useState<Range>(initialDateRange);
 	const onCreateReservation = useCallback(() => {
 		if (!currentUser) {
 			return loginModal.onOpen();
