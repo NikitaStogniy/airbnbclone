1343b798afd8f906693754780e29a913fd033e6a
Nikita Stogniy <ggsailgg@gmail.com>
2023-07-16 17:19:37 +0300

    finish

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
+export const dynamic = "force-dynamic";
+
 import Container from "@/app/components/Container";
 import ListingCard from "./components/Listings/ListingCard";
 import EmptyState from "./components/EmptyState/EmptyState";
 import getListings, { IListingParams } from "@/app/actions/getListings";
 import getCurrentUser from "@/app/actions/getCurrentUser";
 import ClientOnly from "./components/ClientOnly";
+
+interface HomeProps {
+	searchParams: IListingParams;
+}
 
 interface HomeProps {
 	searchParams: IListingParams;
@@ -20,33 +26,35 @@ const Home = async ({ searchParams }: HomeProps) => {
 			</ClientOnly>
 		);
 	}
+
 	return (
 		<ClientOnly>
 			<Container>
 				<div
-					className="pt-24
-          grid
-          grid-cols-1
-          sm:grid-cols-2
-          md:grid-cols-3
-          lg:grid-cols-4
-          xl:grid-cols-5
-          2xl:grid-cols-6
-          gap-8"
+					className="
+			  pt-24
+			  grid 
+			  grid-cols-1 
+			  sm:grid-cols-2 
+			  md:grid-cols-3 
+			  lg:grid-cols-4
+			  xl:grid-cols-5
+			  2xl:grid-cols-6
+			  gap-8
+			"
 				>
-					{listings.map((listing: any) => {
-						return (
-							<ListingCard
-								currentUser={currentUser}
-								key={listing.id}
-								data={listing}
-								disabled={false}
-							/>
-						);
-					})}
+					{listings.map((listing: any) => (
+						<ListingCard
+							currentUser={currentUser}
+							key={listing.id}
+							data={listing}
+							disabled={false}
+						/>
+					))}
 				</div>
 			</Container>
 		</ClientOnly>
 	);
 };
+
 export default Home;
